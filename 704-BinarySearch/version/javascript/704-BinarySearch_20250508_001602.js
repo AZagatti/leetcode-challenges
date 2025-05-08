528e42d3a1af2e304c092de239551d19cfe4620e
André Zagatti <andre.zagatti1@gmail.com>
2025-05-08 00:15:46 -0300

    Create file

---
+++ 704-BinarySearch/version/javascript/704-BinarySearch_20250508_001602.js
@@ -0,0 +1,23 @@
+// Last updated: 08/05/2025, 00:16:02
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+    let left = 0
+    let right = nums.length - 1
+    while (left <= right) {
+        let middle = Math.floor((left + right) / 2)
+        if (nums[middle] === target) {
+            return middle
+        }
+        if (nums[middle] < target) {
+            left = middle + 1
+        }
+        if (nums[middle] > target) {
+            right = middle - 1
+        }
+    }
+    return -1
+};
