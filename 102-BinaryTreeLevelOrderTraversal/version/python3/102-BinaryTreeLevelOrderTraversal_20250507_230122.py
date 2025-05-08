537f25f115d930cbd5b700711407e5caa5c76ff2
André Zagatti <andre.zagatti1@gmail.com>
2025-05-07 23:01:05 -0300

    Create file

---
+++ 102-BinaryTreeLevelOrderTraversal/version/python3/102-BinaryTreeLevelOrderTraversal_20250507_230122.py
@@ -0,0 +1,27 @@
+# Last updated: 07/05/2025, 23:01:22
+# Iterative with queue (popleft() is O(1)).
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        queue = deque([root])
+        result = []
+        while queue:
+            level_size = len(queue)
+            current_level_values = []
+            for i in range(level_size):
+                node = queue.popleft()
+                current_level_values.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(current_level_values)
+        return result
+            
