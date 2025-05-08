2c159c6b1cf1500962a59e89abeb760947640ad5
André Zagatti <andre.zagatti1@gmail.com>
2025-05-07 23:04:14 -0300

    Create file

---
+++ 104-MaximumDepthofBinaryTree/version/javascript/104-MaximumDepthofBinaryTree_20250507_230430.js
@@ -0,0 +1,37 @@
+// Last updated: 07/05/2025, 23:04:30
+// Iterative with queue and index.
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if (root === null ) {
+        return 0
+    }
+    let depth = 0
+    let queue = [root]
+    let headQueueIndex = 0
+    while (queue.length - headQueueIndex) {
+        let levelSize = queue.length - headQueueIndex 
+        for (let i = 0; i < levelSize; i++) {
+            let item = queue[headQueueIndex]
+            headQueueIndex += 1
+            if (item.left) {
+                queue.push(item.left)
+            }
+            if (item.right) {
+                queue.push(item.right)
+            }
+        }
+        depth += 1
+    }
+    return depth
+};
