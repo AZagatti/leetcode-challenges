eaa3f355d65e8d4c536b1345b7e5223e452c3c32
André Zagatti <andre.zagatti1@gmail.com>
2025-05-07 22:09:11 -0300

    Create file

---
+++ 1-TwoSum/version/javascript/1-TwoSum_20250507_220927.js
@@ -0,0 +1,24 @@
+// Last updated: 07/05/2025, 22:09:27
+/*
+ * Good solution.
+ * O(n)
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    let result = []
+    let cache = new Map()
+    nums.forEach((num, i) => {
+        if (result.length === 2) return
+        let cachedNumber = cache.get(target - num)
+        if (cachedNumber !== undefined && cachedNumber !== i) {
+            return result.push(i, cachedNumber)
+        }
+        cache.set(num, i)
+    })
+    return result
+};
