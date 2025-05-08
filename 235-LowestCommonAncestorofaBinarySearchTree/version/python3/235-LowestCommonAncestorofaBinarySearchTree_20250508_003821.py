f97b416b2baf8340169cc6af974ede5c8a82face
André Zagatti <andre.zagatti1@gmail.com>
2025-05-08 00:38:21 -0300

    Create file

---
+++ 235-LowestCommonAncestorofaBinarySearchTree/version/python3/235-LowestCommonAncestorofaBinarySearchTree_20250508_003821.py
@@ -0,0 +1,19 @@
+# Last updated: 08/05/2025, 00:38:21
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def find_lca(node):
+            if node is None:
+                return None
+            if p.val > node.val and q.val > node.val:
+                return find_lca(node.right)
+            if p.val < node.val and q.val < node.val:
+                return find_lca(node.left)
+            return node
+        return find_lca(root)
