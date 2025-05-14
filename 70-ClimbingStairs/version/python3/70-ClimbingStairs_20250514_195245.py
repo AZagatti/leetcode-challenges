7f3fbbfe3c70516843343f1b275af77788a52d2e
André Zagatti <andre.zagatti1@gmail.com>
2025-05-14 19:52:35 -0300

    Create file

---
+++ 70-ClimbingStairs/version/python3/70-ClimbingStairs_20250514_195245.py
@@ -0,0 +1,17 @@
+# Last updated: 14/05/2025, 19:52:45
+# Recursive with cache (memoization).
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        cache = {}
+        def climb(step: int):
+            nonlocal cache
+            if step == 1:
+                return 1
+            if step == 2:
+                return 2
+            if step in cache:
+                return cache[step]
+            result = climb(step - 1) + climb(step - 2)
+            cache[step] = result
+            return result
+        return climb(n)
