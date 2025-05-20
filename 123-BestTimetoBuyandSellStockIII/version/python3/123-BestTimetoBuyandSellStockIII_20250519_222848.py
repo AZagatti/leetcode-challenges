97630d77e8da7fa97b658b2b2ccfa7376fa79a1c
André Zagatti <andre.zagatti1@gmail.com>
2025-05-19 22:28:31 -0300

    Create file

---
+++ 123-BestTimetoBuyandSellStockIII/version/python3/123-BestTimetoBuyandSellStockIII_20250519_222848.py
@@ -0,0 +1,23 @@
+# Last updated: 19/05/2025, 22:28:48
+'''
+-----
+Dynamic Programming with if statement, more performative.
+-----
+'''
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy1 = float('-inf')
+        sell1 = 0
+        buy2 = float('-inf')
+        sell2 = 0
+        for price in prices:
+            if buy1 < -price:
+                buy1 = -price
+            if sell1 < buy1 + price:
+                sell1 = buy1 + price
+            if buy2 < sell1 - price:
+                buy2 = sell1 - price
+            if sell2 < buy2 + price:
+                sell2 = buy2 + price
+        return sell2
