0d46066da67339db32c5462d18bde890934844fb
André Zagatti <andre.zagatti1@gmail.com>
2025-05-20 20:50:16 -0300

    Create file

---
+++ 455-AssignCookies/version/python3/455-AssignCookies_20250520_205036.py
@@ -0,0 +1,20 @@
+# Last updated: 20/05/2025, 20:50:36
+'''
+-----
+Greedy Algorithm with Two Pointers.
+-----
+'''
+
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        i = 0
+        j = 0
+        while i < len(g) and j < len(s):
+            if s[j] >= g[i]:
+                i += 1
+                j += 1
+            else:
+                j += 1
+        return i
