185aca77095e3b5c39c2d2125017c256f0d73a9d
André Zagatti <andre.zagatti1@gmail.com>
2025-05-22 21:14:40 -0300

    Create file

---
+++ 125-ValidPalindrome/version/python3/125-ValidPalindrome_20250522_211502.py
@@ -0,0 +1,21 @@
+# Last updated: 22/05/2025, 21:15:02
+'''
+-----
+Two Pointers.
+-----
+'''
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+            left = 0
+            right = len(s) - 1
+            while left < right:
+                while left < right and not s[left].isalnum():
+                    left += 1
+                while left < right and not s[right].isalnum():
+                    right -= 1
+                if left < right and s[left].lower() != s[right].lower():
+                    return False
+                left += 1
+                right -= 1
+            return True
