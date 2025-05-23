3fa7946d9c2166514eb2978ce99bd5f4ae972434
André Zagatti <andre.zagatti1@gmail.com>
2025-05-22 22:18:41 -0300

    Create file

---
+++ 208-ImplementTrie(PrefixTree)/version/python3/208-ImplementTrie(PrefixTree)_20250522_221904.py
@@ -0,0 +1,33 @@
+# Last updated: 22/05/2025, 22:19:04
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isEndOfWord = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def insert(self, word: str) -> None:
+        current_node = self.root
+        for char in word:
+            if char not in current_node.children:
+                current_node.children[char] = TrieNode()
+            current_node = current_node.children[char]
+        current_node.isEndOfWord = True
+
+    def search(self, word: str) -> bool:
+        current_node = self.root
+        for char in word:
+            if char not in current_node.children:
+                return False
+            current_node = current_node.children[char]
+        return current_node.isEndOfWord
+
+    def startsWith(self, prefix: str) -> bool:
+        current_node = self.root
+        for char in prefix:
+            if char not in current_node.children:
+                return False
+            current_node = current_node.children[char]
+        return True
