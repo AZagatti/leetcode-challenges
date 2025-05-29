8fac6ef5abd3298e6078d3e84f4d3d20703a8228
André Zagatti <andre.zagatti1@gmail.com>
2025-05-28 21:33:09 -0300

    Create 547-NumberofProvinces

---
+++ 547-NumberofProvinces/version/python/547-NumberofProvinces_20250528_213340.py
@@ -0,0 +1,23 @@
+# Last updated: 28/05/2025, 21:33:40
+'''
+-----
+Using DFS.
+-----
+'''
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        visited = set()
+        province_count = 0
+        def dfs(current_city):
+            visited.add(current_city)
+            for neighbor in range(n):
+                if isConnected[current_city][neighbor] == 1 and neighbor not in visited:
+                    dfs(neighbor)
+        for i in range(n):
+            if i not in visited:
+                province_count += 1
+                dfs(i)
+        return province_count
+                
