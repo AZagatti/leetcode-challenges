533d1636ea6612b51580094187f5737bdc42fc77
André Zagatti <andre.zagatti1@gmail.com>
2025-05-29 21:41:56 -0300

    Create 226-InvertBinaryTree

---
+++ 226-InvertBinaryTree/226-InvertBinaryTree.js
@@ -0,0 +1,27 @@
+// Last updated: 29/05/2025, 21:42:28
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+    let queue = [root]
+    while (queue.length) {
+        let item = queue.shift()
+        if (!item) {
+            continue
+        }
+        let tempLeft = item.left
+        item.left = item.right
+        item.right = tempLeft
+        queue.push(item.left, item.right)
+    }
+    return root
+};
