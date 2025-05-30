2337ad83d078cf1e0762cfe69ec64a0384b2928a
André Zagatti <andre.zagatti1@gmail.com>
2025-05-29 21:41:59 -0300

    Create 219-ContainsDuplicateIi

---
+++ 219-ContainsDuplicateIi/219-ContainsDuplicateIi.js
@@ -0,0 +1,20 @@
+// Last updated: 29/05/2025, 21:42:31
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    let result = false
+    let set = new Set()
+    for (let i = 0; i < nums.length; i++) {
+        if (set.size > k) {
+            set.delete(nums[i - k - 1])
+        }
+        if (set.has(nums[i])) {
+            return true
+        }
+        set.add(nums[i])
+    }
+    return result
+};
