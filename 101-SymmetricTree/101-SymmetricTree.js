54ddb7aafd2d39ee134b689a6f576b8ca0f283ee
André Zagatti <andre.zagatti1@gmail.com>
2025-05-29 21:42:22 -0300

    Create 101-SymmetricTree

---
+++ 101-SymmetricTree/101-SymmetricTree.js
@@ -0,0 +1,28 @@
+// Last updated: 29/05/2025, 21:42:55
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function (root) {
+    const isRightLeftSymmetric = (left, right) => {
+        if (!left && !right) {
+            return true
+        }
+        if (left?.val !== right?.val) {
+            return false
+        }
+        let isFirstSymmetric = isRightLeftSymmetric(left.left, right.right)
+        let isRightSymmetric = isRightLeftSymmetric(left.right, right.left)
+        return isFirstSymmetric && isRightSymmetric
+    }
+    const isBothSymmetric = isRightLeftSymmetric(root.left, root.right)
+    return isBothSymmetric
+};
