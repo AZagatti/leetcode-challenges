a1076cfbfcd99f5d61d03be85097ca4120f4efd3
André Zagatti <andre.zagatti1@gmail.com>
2025-05-29 23:07:14 -0300

    Create 49-GroupAnagrams

---
+++ 49-GroupAnagrams/version/python/49-GroupAnagrams_20250529_230747.py
@@ -0,0 +1,16 @@
+# Last updated: 29/05/2025, 23:07:47
+'''
+-----
+Using alphabet list.
+-----
+'''
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
+        return list(res.values())
