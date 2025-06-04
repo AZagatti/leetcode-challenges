479c45cb0520214c6bbe64eabf5c60011ab622a8
André Zagatti <andre.zagatti1@gmail.com>
2025-06-04 01:11:58 -0300

    Create 347-TopKFrequentElements

---
+++ 347-TopKFrequentElements/version/python/347-TopKFrequentElements_20250604_011239.py
@@ -0,0 +1,23 @@
+# Last updated: 04/06/2025, 01:12:39
+'''
+-----
+Using Bucket Sort.
+Time complexity: O(n)
+Space complexity: O(n)
+-----
+'''
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+        result = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                result.append(num)
+                if len(result) == k:
+                    return result
