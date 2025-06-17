765b467c42225af7ae79f49cf7bf52462e6e7fd9
André Zagatti <andre.zagatti1@gmail.com>
2025-06-16 22:19:27 -0300

    Create 205-IsomorphicStrings

---
+++ 205-IsomorphicStrings/version/python/205-IsomorphicStrings_20250616_222027.py
@@ -0,0 +1,21 @@
+# Last updated: 16/06/2025, 22:20:27
+'''
+-----
+Using object and set.
+-----
+'''
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        mapping = {}
+        viewed = set()
+        for i in range(len(s)):
+            if s[i] in mapping and mapping[s[i]] != t[i]:
+                return False
+            if s[i] not in mapping and t[i] in viewed:
+                return False
+            mapping[s[i]] = t[i]
+            viewed.add(t[i])
+        print(mapping, viewed)
+        return True 
+            
