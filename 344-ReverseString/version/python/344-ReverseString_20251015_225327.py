248a75e4267e38ff95e27d483cea24df23a508a2
André Zagatti <andre.zagatti1@gmail.com>
2025-10-15 22:51:04 -0300

    Create 344-ReverseString

---
+++ 344-ReverseString/version/python/344-ReverseString_20251015_225327.py
@@ -0,0 +1,10 @@
+# Last updated: 15/10/2025, 22:53:27
+# Two Pointer Pythonic
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+        
